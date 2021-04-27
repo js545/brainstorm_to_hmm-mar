6b3704db4eabcdd42525f642dd424ef0f591a90f
js545 <j.son5457@gmail.com>
2021-04-27 10:07:20 -0500

    Concatenate data with two samples, then modify to include all files in directory

--- hmm_mar_template.m
+++ hmm_mar_template.m
@@ -1,30 +1,45 @@
-%% Working within HMM-MAR / OSL
+%% Working from raw fif files
 
-data1 = ft_read_data('/home/nebmeg/Documents/HMM_T2DM_Data/006c_ecr_raw_tsss_mc.fif');
-data1 = data1';
-data2 = ft_read_data('/home/nebmeg/Documents/HMM_T2DM_Data/010c_ecr_raw_tsss_mc.fif');
-data2 = data2';
-
-data_concatenate = cat(1, data1, data2);
-T = [size(data1, 1), size(data2, 1)];
-
-options = struct();
-options.K = 5;
-
-hmmmar(data_concatenate, T, options)
+% data1 = ft_read_data('/home/nebmeg/Documents/HMM_T2DM_Data/006c_ecr_raw_tsss_mc.fif');
+% data1 = data1';
+% data2 = ft_read_data('/home/nebmeg/Documents/HMM_T2DM_Data/010c_ecr_raw_tsss_mc.fif');
+% data2 = data2';
+% 
+% data_concatenate = cat(1, data1, data2);
+% T = [size(data1, 1), size(data2, 1)];
+% 
+% options = struct();
+% options.K = 5;
+% 
+% hmmmar(data_concatenate, T, options)
 
 
 %% Working from Brainstorm timeseries outputs
 
+% Loading in files
+myFolder = '/home/nebmeg/Documents/HMM_T2DM_Data/';
+filePattern = fullfile(myFolder, '*.mat');
+theFiles = dir(filePattern);
+
 K=10; %Number of states
 Fs = 1000; %Sampling frequency
 
+% Load multiple datasets using Brainstorm interface
+
+% Load multiple datasets
 data = load('/home/nebmeg/Documents/HMM_T2DM_Data/timeseries/converted/006c_DK_scout_timeseries_epochs_removed');
+data2 = load('/home/nebmeg/Documents/HMM_T2DM_Data/timeseries/converted/010c_DK_scout_timeseries_epochs_removed');
 % Invert Brainstorm format to match HMM-MAR
 timeseries = data.d_full;
+timeseries2 = data2.d_full;
+
+% Concatenate datasets
+
+timeseries = cat(1, timeseries, timeseries2);
 
 % Select data subset just for training / testing
 timeseries = timeseries(1:80000, 1:3);
+
 
 %T = size(timeseries, 1);
 T = 4000 * ones(size(timeseries,1)/4000,1);
@@ -53,6 +68,7 @@ options_mt.tapers = [4 7];
 options_mt.p = 0;
 options_mt.win = 500;
 options_mt.order = 2;
+
 spectral_info = hmmspectramt(timeseriess,T,Gamma,options_mt);
 
 plot_hmmspectra(spectral_info);
